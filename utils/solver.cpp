8cf5489c37aa3e1a56e36f29950401f15d464d87
Nikita Nechepurenko <rufflerock@gmail.com>
2021-05-23 14:16:00 +0300

    add solver interface and euler solver class

--- utils/solver.cpp
+++ utils/solver.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "solver.h"
+
+State operator*(double h, State state) {
+    return state;
+}
+
+EulerSolver::EulerSolver(std::function<State(State)> rhsFunction_,
+                         std::function<void(State)> onUpdateConsumer_,
+                         double h_, int maxIterations_)
+        :
+        onUpdateConsumer(std::move(onUpdateConsumer_)),
+        rhsFunction(std::move(rhsFunction_)),
+        h(h_),
+        maxIterations(maxIterations_) {}
+
+void EulerSolver::solve(State initialState) {
+    State currentState = initialState;
+    for (int iteration = 0; iteration < maxIterations; iteration++) {
+        onUpdateConsumer(currentState);
+        previousStates.push(step());
+    }
+}
+
+State EulerSolver::step() {
+    State nextState = h * rhsFunction(previousStates.back());
+    previousStates.pop();
+    return nextState;
+}
--- utils/solver.h
+++ utils/solver.h
@@ -5,12 +5,50 @@
 #ifndef SPBETU_DYNAMICSYSTEMS_COURSEWORK_SOLVER_H
 #define SPBETU_DYNAMICSYSTEMS_COURSEWORK_SOLVER_H
 
+#include <functional>
+#include <queue>
+#include <utility>
 
 
-class solver {
+class State {
+
+public:
 
 };
 
+class Solver {
+public:
+    virtual void solve(State initialState) = 0;
+};
+
+class EulerSolver : public Solver {
+    std::queue<State> previousStates;
+    std::function<void(State)> onUpdateConsumer;
+    std::function<State(State)> rhsFunction;
+    double h;
+    int maxIterations = 0;
+protected:
+    State step();
+
+//    EulerSolver(const EulerSolver &es) {
+//        if (&es != this) {
+//            previousStates = es.previousStates;
+//            onUpdateConsumer = es.onUpdateConsumer;
+//            rhsFunction = es.rhsFunction;
+//            h = es.h;
+//            maxIterations = es.maxIterations;
+//        }
+//    }
+
+public:
+    EulerSolver(
+            std::function<State(State)> rhsFunction_,
+            std::function<void(State)> onUpdateConsumer_,
+            double h_ = 0.01, int maxIterations_ = 10000);
+
+
+    void solve(State initialState) override;
+};
 
 
 #endif //SPBETU_DYNAMICSYSTEMS_COURSEWORK_SOLVER_H
