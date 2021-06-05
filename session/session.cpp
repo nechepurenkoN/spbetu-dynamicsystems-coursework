7950eee5139bd5d0649b0ce74cd7e5ddb3ed88e9
Nikita Nechepurenko <rufflerock@gmail.com>
2021-06-05 21:31:29 +0300

    consequencial start

--- session/session.cpp
+++ session/session.cpp
@@ -5,20 +5,22 @@
 #include "session.h"
 
 void solverRunnable(std::vector<Particle*> particles, std::shared_ptr<RhsFunction> ptr) {
-    auto particle = particles.back();
-    auto s = std::shared_ptr<Solver>(new RK4Solver(
-            ptr,
-            [&particle](State state) -> void {
-                particle->setX(state.coordinate.x);
-                particle->setY(state.coordinate.y);
-                particle->setZ(state.coordinate.z);
-                std::this_thread::sleep_for(std::chrono::milliseconds(10));
-            }, 0.01, 10000
-    ));
-    State state(Point3D(particle->getX(), particle->getY(), particle->getZ()), Point3D(0, 0, 0), 1);
-    try {
-        s->solve(state);
-    } catch (int &e){}
+    for (auto &particle : particles) {
+        auto s = std::shared_ptr<Solver>(new RK4Solver(
+                ptr,
+                [&particle](State state) -> void {
+                    particle->setX(state.coordinate.x);
+                    particle->setY(state.coordinate.y);
+                    particle->setZ(state.coordinate.z);
+                    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                }, 0.01, 10000
+        ));
+        State state(Point3D(particle->getX(), particle->getY(), particle->getZ()), Point3D(0, 0, 0), 1);
+        try {
+            s->solve(state);
+        } catch (int &e){}
+    }
+
 }
 
 void Session::start(std::vector<Particle*> &particles, std::shared_ptr<RhsFunction> ptr) {
